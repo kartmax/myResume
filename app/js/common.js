befad1037fc87330a2a77e3670aef3ed5592bcd9
Kartel Maksym <kartmax1804@gmail.com>
2021-09-05 13:46:26 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,196 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Custom JS
+
+    // =======================================
+    // ============ preloader=================
+    // =======================================
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+
+
+    // =======================================
+    // ============ progress skill ===========
+    // =======================================  
+    let skills = document.querySelectorAll('.js-skill')
+    for(skill of skills) {
+        skill.classList.add('wow')
+        skill.classList.add('fadeIn')
+        skill.setAttribute('data-wow-duration', '1s');
+        skill.setAttribute('data-wow-offset', '25');
+    }
+
+    let wowSkill = new WOW({   
+        boxClass:     'js-skill',
+        callback: function(skill) {
+            let skillProgresEl = skill.querySelector('.skill__progress_value')
+            let valueSkill = skillProgresEl.getAttribute('data-value')
+            skillProgresEl.style.width = valueSkill
+        }   
+    }).init()
+   
+    
+    // ===========================================
+    // ============ localization ru en ===========
+    // ===========================================
+    const btn_ru = document.querySelector('.js-ru');
+    const btn_en = document.querySelector('.js-en');
+
+    const variantBrowserLangForRu = ['ru', 'ru-ru',  'uk-uk', 'uk-ua', 'uk', 'be-be', 'be']
+    const ru_val = 'ru'
+    const en_val = 'en'
+    let interfaceLang
+
+    const contentSearch = {
+        ru : {
+            about_me_1 : 'В 2017 году начал изучать html, css и  jquery для трудоустройства на позицию html-верстальщик. За время работы сверстал около 30 проектов (лендинги, корпоративные сайты, блоги).',
+            about_me_2 : 'В последних двух проектах был на позиции front-end разработчика, где помимо верстки занимался построением логики работы веб-приложений (связка клиент-сервер).',
+            
+            study_1 : 'Курсы по html и css',
+            study_2 : 'Учебник по JavaScript',
+            study_3 : 'Курсы по html-верстке, веб-дизайну, gulp, scss',
+            study_4 : 'Курсы по jQuery, JavaScript',
+            study_5 : 'Курсы по git, linux, docker, ...',
+
+            experience_1 : 'Верстка лендингов, корпоративных сайтов.',
+            experience_2 : 'Верстка лендингов, корпоративных сайтов, немного насадка на WordPress.',
+            experience_3 : 'Верстка сайтов-блогов, корпоративных сайтов.',
+            experience_5 : 'Front-end, настройка LAMP, управление доменами, написание скриптов на python (автоматизация процессов обработки датасетов с изображениями, парсер), обучение нейронной сети, дизайн мобильных приложений и веб-сайтов, релиз мобильных приложений в AppStore и PlayMarket...',
+
+            my_works_1 : 'Многостраничный сайт для обзора криптовалютных бирж.',
+            my_works_2 : 'Многостраничный сайт-блог.',
+            my_works_3 : 'Корпоративный сайт строительной компании.',
+            my_works_4 : 'Лендинг для пивоварни.',
+            my_works_5 : 'Главная страница сайта с расширенным фильтром для записи к мастеру.',
+            my_works_6 : 'Шапка сайта в виде анимированного слайдера.',
+        },
+
+        en : {
+            about_me_1 : 'In 2017, I started studying html, css and jquery for employment as an html layout designer. During my work, I have made up about 30 projects (landing pages, corporate sites, blogs).',
+            about_me_2 : 'In the last two projects, I was a front-end developer, where, in addition to layout, I was engaged in building the logic of web applications (client-server link).',
+            
+            study_1 : 'Html and css courses',
+            study_2 : 'JavaScript tutorial',
+            study_3 : 'Courses in html layout, web design, gulp, scss',
+            study_4 : 'JQuery, JavaScript courses',
+            study_5 : 'Courses on git, linux, docker, ...',
+
+            experience_1 : 'Layout of landing pages, corporate sites.',
+            experience_2 : 'Layout of landing pages, corporate sites, a little bit of WordPress add-on.',
+            experience_3 : 'Layout of blog sites, corporate sites.',
+            experience_5 : 'Front-end, setting up LAMP, managing domains, writing scripts in python (automating processes for processing datasets with images, parser), training a neural network, designing mobile applications and websites, releasing mobile applications in AppStore and PlayMarket ...',
+
+            my_works_1 : 'Multi-page site for an overview of cryptocurrency exchanges.',
+            my_works_2 : 'Multi-page blog site.',
+            my_works_3 : 'The corporate website of the construction company.',
+            my_works_4 : 'Landing page for a brewery.',
+            my_works_5 : 'The main page of the site with an advanced filter for writing to the master.',
+            my_works_6 : 'Header of the site in the form of an animated slider.',
+        }
+    }
+    
+    function ChangeValInterfaceLang() {
+        // if the language has not been selected manually
+        if (typeof localStorage.lang === 'undefined') {
+            let browserLang = navigator.language || navigator.userLanguage;
+            browserLang = browserLang.toLowerCase()
+            for(variant of variantBrowserLangForRu) {
+                // for Russian speakers
+                if(browserLang === variant) {
+                    btn_en.classList.remove('active')
+                    btn_ru.classList.add('active')
+                    interfaceLang = ru_val
+                    break
+                }
+                // for English speaking
+                else {
+                    btn_ru.classList.remove('active')
+                    btn_en.classList.add('active')
+                    interfaceLang = en_val
+                }
+            }
+        } 
+        // if the language was selected manually
+        else {
+            if(localStorage.lang == ru_val) {
+                interfaceLang = ru_val
+                btn_en.classList.remove('active')
+                btn_ru.classList.add('active')
+            } else {
+                interfaceLang = en_val
+                btn_ru.classList.remove('active')
+                btn_en.classList.add('active')
+            }
+        }
+    }
+    function changeLangContent () {
+        let langChangeItems = document.querySelectorAll('.js-lang-change-el')
+        for (item of langChangeItems) {
+            if(interfaceLang == ru_val) {
+                item.innerText = item.getAttribute('data-lang_ru')
+            }
+            if(interfaceLang == en_val) {
+                item.innerText = item.getAttribute('data-lang_en')
+            }
+        }
+    }
+    function RenderContentSearch() {
+        const contentSearchItems = document.querySelectorAll('.js-lang-change-search')
+        for (item of contentSearchItems) {
+            let valDataLangItem = item.getAttribute('data-content-search')
+            item.innerText = contentSearch[interfaceLang][valDataLangItem]
+        }
+    }
+
+    function logicChangeLang() {
+        ChangeValInterfaceLang()
+        changeLangContent()
+        RenderContentSearch()
+    }
+    logicChangeLang()
+
+    btn_ru.addEventListener('click', function() {
+        if (this.classList.contains('active') == false) {
+            localStorage.setItem('lang', ru_val)
+            logicChangeLang()
+        }
+    })
+
+    btn_en.addEventListener('click', function() {
+        if (this.classList.contains('active') == false) {
+            localStorage.setItem('lang', en_val)
+            logicChangeLang()
+        }
+    })
+
+
+
+
+    // Custom jQ
+
+    //-----for scroll to top------
+    // hide #back-top first
+    $(".scrolltop").hide();
+
+    // fade in #back-top
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.scrolltop').fadeIn();
+        } else {
+            $('.scrolltop').fadeOut();
+        }
+    });
+
+    // scroll body to 0px on click
+    $('.scrolltop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+    });
+    //-----END for scroll to top------
+
+
+
+})
---
+++ gulpfile.js
@@ -0,0 +1,169 @@
+const { src, dest, parallel, series, watch } = require('gulp')
+
+const sass           = require('gulp-sass')(require('sass'))
+const browserSync    = require('browser-sync').create()
+const concat         = require('gulp-concat')
+const terser         = require('gulp-terser')
+const cleanCSS       = require('gulp-clean-css')
+const del            = require('del')
+const imagemin       = require('gulp-imagemin')
+const newer          = require('gulp-newer')
+const cache          = require('gulp-cache')
+const autoprefixer   = require('gulp-autoprefixer')
+const bourbon        = require('node-bourbon')
+const notify         = require('gulp-notify')
+const spritesmith	 = require('gulp.spritesmith')
+const svgSprite      = require('gulp-svg-sprites')
+const sourcemaps 	 = require('gulp-sourcemaps')
+const rev            = require('gulp-rev')
+const revReplace     = require('gulp-rev-replace')
+const ftp            = require('vinyl-ftp')
+const gutil          = require('gulp-util')
+
+function scripts () {
+	return src ([
+		'app/libs/jquery/dist/jquery.min.js',
+        // 'app/libs/slimmenu-master/dist/js/jquery.slimmenu.min.js',
+        'app/libs/wow/wow.min.js',
+        //'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+        //'app/libs/owl.carousel/dist/owl.carousel.min.js',
+        //'app/libs/jquery.ellipsis-master/jquery.ellipsis.min.js',
+        //'app/libs/jQueryFormStyle/jquery.formstyler.min.js',
+        //'app/libs/malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.concat.min.js',
+        //'app/libs/snap.svg/snap.svg-min.js',
+		//'app/libs/TouchSwipe/jquery.touchSwipe.min.js',
+		//'app/libs/cropbox/jquery.cropbox.js',
+		//'app/libs/exif.js/exif.js',
+		//'app/libs/clipboard.js/dist/clipboard.min.js',
+		//'app/libs/mixtup/mixitup.min.js',
+		//'app/libs/Gsap/gsap.min.js',
+		//'app/libs/micromodal/micromodal.min.js', //https://micromodal.vercel.app/
+		// 'app/libs/Swiper/swiper-bundle.min.js',
+
+		'app/js/common.js'
+	])
+	.pipe(concat('scripts.min.js'))
+	.pipe(terser())
+	.pipe(dest('app/js'))
+	.pipe(browserSync.stream())
+}
+
+function spritepng () {
+	var spriteData = src('app/img/for-sprite-png/*.png').pipe(spritesmith({
+        imgName: 'sprite-png.png',
+        cssName: '_sprite-png.scss',
+        padding: 10
+    }));
+    return spriteData.pipe(dest('app/img'));
+}
+
+function spritesvg () {
+	return src('app/img/for-sprite-svg/*.svg')
+	.pipe(svgSprite({
+		mode: "symbols"
+	}))
+	.pipe(dest("app/img/"));
+}
+
+function browsersync () {
+    browserSync.init({
+        server : { baseDir : 'app/' },
+        notify : false, // отключение уведомления
+        online : true // работа без wi-fi сети (false -- без wi-fi)
+    })
+}
+
+function func_sass () {
+	return src('app/sass/**/*.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		includePaths: bourbon.includePaths
+	}).on("error", notify.onError()))
+	.pipe(concat('app.min.css'))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS())
+	.pipe(sourcemaps.write('.'))
+	.pipe(dest('app/css'))
+	.pipe(browserSync.stream())
+}
+ 
+function func_imagemin () {
+	return src(['app/img/**/*+(.png|jpg|jpeg|ico|gif)', 'app/img/**/*.svg', '!app/img/for-sprite-svg/*', '!app/img/for-sprite-png/*'])
+	.pipe(newer('app/img/**/*+(.png|jpg|jpeg|ico|gif)', 'app/img/**/*.svg', '!app/img/for-sprite-svg/*', '!app/img/for-sprite-png/*'))
+	.pipe(cache(imagemin()))
+	.pipe(dest('dist/img'))
+}
+
+function revScript () {
+	return src('app/js/scripts.min.js')
+	.pipe(rev())
+	.pipe(dest('dist/js'))
+	.pipe(rev.manifest('js.json'))
+	.pipe(dest('manifest'))
+}
+function revStyle () {
+	return src('app/css/app.min.css')
+	.pipe(rev())
+	.pipe(dest('dist/css'))
+	.pipe(rev.manifest('css.json'))
+	.pipe(dest('manifest'))
+}
+
+function buildcopy () {
+	return src([
+		'app/**/*.html',
+		'!app/img/*.html',
+		'app/fonts/**/*',
+		'app/*.htaccess',
+		'app/*.php',
+	], {base : 'app'})
+	.pipe(revReplace({ manifest: src('manifest/js.json') }))
+	.pipe(revReplace({ manifest: src('manifest/css.json') }))
+	.pipe(dest('dist/'))
+}
+
+function removedist () {
+	return del('dist');
+}
+
+function clearcache () {
+	return cache.clearAll();
+}
+
+
+function func_watch () {
+	watch('app/sass/**/*.scss', func_sass)
+	watch(['libs/**/*.js', 'app/js/common.js'], scripts)
+    watch('app/**/*.html').on('change', browserSync.reload)
+}
+ 
+function deploy () {
+	var conn = ftp.create({
+		host:      'hostname.com',
+		user:      'username',
+		password:  'userpassword',
+		parallel:  10,
+		log: gutil.log
+	});
+
+	var globs = [
+		'dist/**',
+		'dist/*.htaccess',
+	];
+	return gulp.src(globs, {buffer: false})
+		.pipe(conn.dest('/path/to/folder/on/server'));
+}
+
+
+
+
+exports.spritepng  = spritepng
+exports.spritesvg  = spritesvg
+exports.buildcopy  = buildcopy
+exports.removedist = removedist
+exports.clearcache = clearcache
+exports.deploy = deploy
+
+exports.build = series(removedist, func_imagemin, func_sass, scripts, revScript, revStyle, buildcopy)
+
+exports.default = parallel(func_sass, scripts, browsersync, func_watch)
